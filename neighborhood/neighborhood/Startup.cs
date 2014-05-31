0cfe0aed448f7d57327c08d71cf6e2fb63fea8e2
Robert McCardell <mccardellr@gmail.com>
2014-05-31 14:51:13 -0500

    Initial commit
    Committing mvc initial project

---
+++ neighborhood/neighborhood/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(neighborhood.Startup))]
+namespace neighborhood
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
